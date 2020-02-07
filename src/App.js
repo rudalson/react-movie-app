66d1cbc1377a611de737c26154547fa679ae6250
rudalson <rudalson@gmail.com>
2020-02-07 18:47:32 +0900

    added #2 4 Protection with PropTypes
    propType 패키지 설치 후 사용법 예시

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name, pic}) {
+const foodILike = [
+    {
+        id: 1,
+        name: "Kimchi",
+        image: "https://www.rotinrice.com/wp-content/uploads/2011/09/NapaCabbageKimchi-1.jpg",
+        rating: 5
+    },
+    {
+        id: 2,
+        name: "Samgyopsal",
+        image: "https://image.ajunews.com/content/image/2017/01/25/20170125102649752096.jpg",
+        rating: 4.9
+    },
+    {
+        id: 3,
+        name: "Bibimbap",
+        image: "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjRfMjI3/MDAxNDkzMDIzMjc4MjU2.L-3Vv9r0XjeRGGncaB0p0II6mw9-NoBfu2k4PMCrTdgg.jP8wA64wrWrXrH3ZTP4UBCPR6ZWppqqnhXkS8FPpYMQg.JPEG.estelle926/151435979-56a57a083df78cf772888a61.jpg?type=w800",
+        rating: 4.8
+    }
+];
+
+function Food({name, pic, rating}) {
     return (
         <div>
             <h2>I like {name}</h2>
-            <img src={pic}/>
+            <h4>{rating}/5.0</h4>
+            <img src={pic} alt={name}/>
         </div>
     )
 }
 
-const foodILike = [
-    {
-        name: "Kimchi",
-        image: "https://www.rotinrice.com/wp-content/uploads/2011/09/NapaCabbageKimchi-1.jpg"
-    },
-    {
-        name: "Samgyopsal",
-        image: "https://image.ajunews.com/content/image/2017/01/25/20170125102649752096.jpg"
-    }
-];
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    pic: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
+};
 
 function renderFood(dish) {
-    return <Food name={dish.name} pic={dish.image}/>
+    return <Food
+        key={dish.id}
+        name={dish.name}
+        pic={dish.image}
+        rating={dish.rating}
+    />
 }
 
 function App() {
