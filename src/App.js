68f657dbe91d1b1bba4d628c8fba14c817b92fd5
rudalson <rudalson@gmail.com>
2020-02-07 19:26:29 +0900

    added #3 1 All you need to know about State
    state를 이해할 수 있게 불필요한 것들은 정리하고 App를 class형으로 변경

--- src/App.js
+++ src/App.js
@@ -1,58 +1,28 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
-const foodILike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        image: "https://www.rotinrice.com/wp-content/uploads/2011/09/NapaCabbageKimchi-1.jpg",
-        rating: 5
-    },
-    {
-        id: 2,
-        name: "Samgyopsal",
-        image: "https://image.ajunews.com/content/image/2017/01/25/20170125102649752096.jpg",
-        rating: 4.9
-    },
-    {
-        id: 3,
-        name: "Bibimbap",
-        image: "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjRfMjI3/MDAxNDkzMDIzMjc4MjU2.L-3Vv9r0XjeRGGncaB0p0II6mw9-NoBfu2k4PMCrTdgg.jP8wA64wrWrXrH3ZTP4UBCPR6ZWppqqnhXkS8FPpYMQg.JPEG.estelle926/151435979-56a57a083df78cf772888a61.jpg?type=w800",
-        rating: 4.8
+class App extends React.Component {
+    state = {
+        count: 0
+    };
+
+    add = () => {
+        console.log("add");
+    };
+
+    minus = () => {
+        console.log("minus");
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>The number is : {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
     }
-];
-
-function Food({name, pic, rating}) {
-    return (
-        <div>
-            <h2>I like {name}</h2>
-            <h4>{rating}/5.0</h4>
-            <img src={pic} alt={name}/>
-        </div>
-    )
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    pic: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired
-};
-
-function renderFood(dish) {
-    return <Food
-        key={dish.id}
-        name={dish.name}
-        pic={dish.image}
-        rating={dish.rating}
-    />
-}
-
-function App() {
-    return (
-        <div>
-            {foodILike.map(renderFood)}
-        </div>
-    )
 }
 
 export default App;
+
