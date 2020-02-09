db613b5f603c777894d2d2683341659fa250b33b
rudalson <rudalson@gmail.com>
2020-02-09 23:55:59 +0900

    Updates

---
+++ precache-manifest.8a2c7325fdb49d4d3ad4714d8876be2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc82191639fcbc831ee6aecf07e3f36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9a761d24b0ef097d1c8",
+    "url": "/static/css/main.1f5aa760.chunk.css"
+  },
+  {
+    "revision": "77c0ea1e5e1b7f5c60bb",
+    "url": "/static/js/2.4e9ea28a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4e9ea28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a761d24b0ef097d1c8",
+    "url": "/static/js/main.ac0df07e.chunk.js"
+  },
+  {
+    "revision": "8993a72302fff8f4d7e0",
+    "url": "/static/js/runtime-main.9ee8e355.js"
+  }
+]);
